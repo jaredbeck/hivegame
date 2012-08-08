965fa78f58673aa574e04dc27109d482cdf310a9
Jared Beck <jared@jaredbeck.com>
2012-08-08 01:50:13 -0400

    enumerable hive

--- lib/hivegame/hive.rb
+++ lib/hivegame/hive.rb
@@ -1,12 +1,19 @@
 module Hivegame
   class Hive
+    include Enumerable
+
     def initialize
       @bugs = []
+    end
+
+    def each
+      @bugs.each {|b| yield b}
     end
 
     def empty?
       @bugs.empty?
     end
+
   end
 end
 
--- spec/hive_spec.rb
+++ spec/hive_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe Hivegame::Hive do
   context "when initialized" do
     it("is empty") { should be_empty }
+    it("has zero bugs") { subject.count.should == 0 }
   end
 end
 
