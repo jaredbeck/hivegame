e691efb5ed5a093a73c1394c387a8f62570a9e4b
Jared Beck <jared@jaredbeck.com>
2012-08-13 11:41:15 -0400

    use dictionary instead of cubic array

--- lib/hivegame/board.rb
+++ lib/hivegame/board.rb
@@ -11,26 +11,24 @@ module Hivegame
     end
   end
 
- class Board
-   def initialize(rows=20, cols=20)
-     @rows, @cols = rows, cols
-     @board = Array.new(@rows) do |row|
-       Array.new(@cols) do |col|
-         Hex.new
-       end
-     end
-   end
+  # A Hive board is a three-dimensional matrix of hexagonal cells,
+  # which are identified by a 3-tuple of coordinates.
+  class Board
+    def initialize
+      @board = {[0,0,0] => Hex.new}
+    end
 
-   def add(r,c,h, bug)
-     return false unless (0..@rows).include? r
-     return false unless (0..@cols).include? c
-     return false if h > 1 && @board[r][c].occupied? == false
-     return false if @board[r][c].occupied? 
+    def add(r,c,h, bug)
+      if h > 1
+        hex_below = hex([r,c,h-1])
+        return false unless hex_below.occupied?
+      end
+      hex = hex([r,c,h])
+      return false if hex.occupied?
 
-     @board[r][c].bug = bug
-     return true 
-   end
-
+      @board[[r,c,h]].bug = bug
+      return true
+    end
 
    # This will print the board out to the console
    def draw
@@ -51,14 +49,20 @@ module Hivegame
      true
    end
 
-   def neighbors(row, col)
-     [[-1,-1],[-1,0],[0,-1],[0,1],[1,0],[1,1]].map do |r, c|
-       [row+r, col+c]
-     end
-   end
+    def hex point
+      return ArgumentError unless point.is_a? Array
+      @board[point] = Hex.new if @board[point].nil?
+      @board[point]
+    end
 
-   def [](row)
-     @board[row]
-   end
- end
-end 
+    def neighbors point
+      return ArgumentError unless point.is_a? Array
+      row, col, height = point[0], point[1], point[2]
+      offsets = [[-1,-1,0], [-1,0,0], [0,-1,0], [0,1,0], [1,0,0], \
+        [1,1,0], [0,0,1], [0,0,-1]]
+      offsets.map do |r,c,h|
+        [row+r, col+c, height+h]
+      end
+    end
+  end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,28 +2,33 @@ require "spec_helper"
 
 describe Hivegame::Board do
 
-  context "when initialized on a 10x10 grid" do
+  context "when initialized" do
     it("is empty") { should be_empty }
-  end 
+  end
 
   describe "#neighbors" do
-    it "shows six neighbors for a middle point" do
-      subject.neighbors(5,5).should have(6).neighbors
+    it "returns eight for any point" do
+      r = rand(10000)
+      subject.neighbors([r,r,r]).should have(8).neighbors
     end
-  end 
+  end
 
   describe "#add" do
     it "adds a piece to the given position" do
       subject.add(5,5,1, double("Bug")).should be_true
     end
+    it "can add a piece very far away from other pieces" do
+      subject.add(100, 100, 1, double("Bug")).should be_true
+      subject.add(-100, -100, 1, double("Bug")).should be_true
+    end
     it "fails if the position is occupied" do
       subject.add(5,5,1, double("Bug")).should be_true
       subject.add(5,5,1, double("Bug")).should be_false
     end
-    it "fails if its adding to a z-level with no bug underneath" do
+    it "fails to add to a z-level with no bug underneath" do
       subject.add(5,5,2, double("Bug")).should be_false
     end
-    it "can add to a z-level if a bug is underneath and the bug is a beetle.  Cuz shyeah" 
+    it "can add to a z-level if a bug is underneath and the bug is a beetle"
     it "increases the count"
   end
 
