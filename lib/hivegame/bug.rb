888ba1f31cf17148dceecee84f51b9e9ca7f1263
Jared Beck <jared@jaredbeck.com>
2012-12-22 23:22:44 -0500

    add `move_limit` to bugs

--- lib/hivegame/bug.rb
+++ lib/hivegame/bug.rb
@@ -1,6 +1,37 @@
 module Hivegame
   class Bug
+    attr_accessor :move_limit
     def color
+    end
+  end
+
+  class Ant < Bug
+    def initialize
+      @move_limit = Float::INFINITY
+    end
+  end
+
+  class Bee < Bug
+    def initialize
+      @move_limit = 1
+    end
+  end
+
+  class Beetle < Bug
+    def initialize
+      @move_limit = 1
+    end
+  end
+
+  class Grasshopper < Bug
+    def initialize
+      @move_limit = Float::INFINITY
+    end
+  end
+
+  class Spider < Bug
+    def initialize
+      @move_limit = 3
     end
   end
 end
--- spec/bug_spec.rb
+++ spec/bug_spec.rb
@@ -12,3 +12,23 @@ describe Hivegame::Bug do
   end
 end
 
+describe Hivegame::Ant do
+  its(:move_limit) { should == Float::INFINITY }
+end
+
+describe Hivegame::Bee do
+  its(:move_limit) { should == 1 }
+end
+
+describe Hivegame::Beetle do
+  its(:move_limit) { should == 1 }
+end
+
+describe Hivegame::Grasshopper do
+  its(:move_limit) { should == Float::INFINITY }
+end
+
+describe Hivegame::Spider do
+  its(:move_limit) { should == 3 }
+end
+
